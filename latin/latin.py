18e69ef0e3bb81dac06c7c8b0ca755c0d3b6da24
zbu <zbu@users.noreply.github.com>
2018-03-30 21:23:34 -0400

    Add files via upload

---
+++ latin/latin.py
@@ -0,0 +1,32 @@
+import discord
+from discord.ext import commands
+try: # check if BeautifulSoup4 is installed
+    from bs4 import BeautifulSoup
+    soupAvailable = True
+except:
+    soupAvailable = False
+import aiohttp
+
+class nsextras:
+    """NS Extras"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def rmbpost(self, word):
+        """Translates from English to Latin"""
+
+        #Your code will go here
+        url = "http://archives.nd.edu/cgi-bin/wordz.pl?english=" + word #build the web adress
+        async with aiohttp.get(url) as response:
+            soupObject = BeautifulSoup(await response.text(), "html.parser")
+        try:
+            text = soupObject.find('pre').get_text()
+            text2 = text.split(',')[0]
+            await self.bot.say(text2)
+        except:
+            await self.bot.say("Word doesn't exist.")
+
+def setup(bot):
+    bot.add_cog(lingtools(bot))
