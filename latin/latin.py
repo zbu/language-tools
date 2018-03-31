8858fe252fc94f675b658aba3951e9be2dc0735e
zbu <zbu@users.noreply.github.com>
2018-03-30 22:31:45 -0400

    Add files via upload

--- latin/latin.py
+++ latin/latin.py
@@ -13,36 +13,23 @@ class lingtools:
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.group(name="latin", pass_context=True)
-    async def _latin(self, ctx):
+    @commands.command()
+    async def latin(self, word):
         """Translates from English to Latin"""
 
-        if ctx.invoked_subcommand is None:
-            await send_cmd_help(ctx)
-    @_latin.command()
-    async def normal(self, word):
         #Your code will go here
         url = "http://archives.nd.edu/cgi-bin/wordz.pl?english=" + word #build the web adress
         async with aiohttp.get(url) as response:
             soupObject = BeautifulSoup(await response.text(), "html.parser")
         try:
             text = soupObject.find('pre').get_text()
-            text2 = text.split(',')[0]
-            await self.bot.say(text2)
+            text2 = text.split(' ')[0]
+            if ',' in text2:
+                text2 = text.split(',')[0]
+            else:
+                await self.bot.say(text2)
         except:
             await self.bot.say("Word doesn't exist.")
-    @_latin.command()
-    async def reverse(self, word):
-        #Your code will go here
-        url = "http://http://archives.nd.edu/cgi-bin/wordz.pl?keyword=" + word #build the web adress
-        async with aiohttp.get(url) as response:
-            soupObject = BeautifulSoup(await response.text(), "html.parser")
-        try:
-            text = soupObject.find('pre').get_text()
-            text2 = text.split('X]')[1]
-            text3 = text2.split(',')[0]
-            await self.bot.say(text3)
-        except:
-            await self.bot.say("Word doesn't exist.")
+
 def setup(bot):
-    bot.add_cog(lingtools(bot))
+bot.add_cog(lingtools(bot))
